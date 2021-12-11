1ff9af5fdcd8fc2ba4bbe4a927b7a0094a4b516a
Pedro <pedrofphenriques@hotmail.com>
2021-12-11 16:09:03 -0100

    node zip

--- NodeWebServerForPython-main/webserver.js
+++ NodeWebServerForPython-main/webserver.js
@@ -121,9 +121,9 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
 	socket.on('rgb', function(data) {
 		if (data.check == true){
 			console.log("true")
-			redLED.pwmWrite(data.rgbvalue[0]*data.alpha);
-			greenLED.pwmWrite(data.rgbvalue[1]*data.alpha);
-			blueLED.pwmWrite(data.rgbvalue[2]*data.alpha);
+			redLED.pwmWrite(Math.round(data.rgbvalue[0]*data.alpha));
+			greenLED.pwmWrite(Math.round(data.rgbvalue[1]*data.alpha));
+			blueLED.pwmWrite(Math.round(data.rgbvalue[2]*data.alpha));
 		}else{
 			console.log("false")
 
